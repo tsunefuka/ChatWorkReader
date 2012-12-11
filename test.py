3cf1c9d8e02f0a3867cf81e21f5063dce7b7154f
poor729 <std0dc00@gmail.com>
2012-12-12 00:43:55 +0900

    ログインして中身とれったっぽい。中身の精査してないがな！

--- test.py
+++ test.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python
 
-import urllib
+import urllib, urllib2, cookielib, re
 
-url = "https://kcw.kddi.ne.jp/#!rid1927770"
-f = urllib.urlopen(url)
-html = f.read()
+url = "https://kcw.kddi.ne.jp/login.php"
+
+cj = cookielib.CookieJar()
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+
+mail     = 'tsunefuka-k@klab.com'
+password = 'Eucharis729'
+r = opener.open(url, "email=%s&password=%s" % (mail, password))
 
 print "Content-type: text/html; \n\n"
+print 'success'
+
+url2 = "https://kcw.kddi.ne.jp/#!rid1927770"
+f = opener.open(url2)
+#f = urllib.urlopen(url)
+html = f.read()
 print html
 
