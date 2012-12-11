0fe1cb580eb14147d6c7e4a530aaa4b2b077c764
poor729 <std0dc00@gmail.com>
2012-12-12 08:49:57 +0900

    ユーザーエージェント偽装した

--- test.py
+++ test.py
@@ -2,19 +2,24 @@
 
 import urllib, urllib2, cookielib, re
 
+# url for login
 url = "https://kcw.kddi.ne.jp/login.php"
 
 cj = cookielib.CookieJar()
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+opener.addheaders = [('User-agent', 'Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/1C28 Safari/419.3')]
+urllib2.install_opener(opener)
 
-mail     = 'tsunefuka-k@klab.com'
-password = 'Eucharis729'
+# mail and pass
+mail     = 'mail'
+password = 'password'
 r = opener.open(url, "email=%s&password=%s" % (mail, password))
 
 print "Content-type: text/html; \n\n"
 print 'success'
 
 url2 = "https://kcw.kddi.ne.jp/#!rid1927770"
+#url2 = "https://kcw.kddi.ne.jp/#"
 f = opener.open(url2)
 #f = urllib.urlopen(url)
 html = f.read()
